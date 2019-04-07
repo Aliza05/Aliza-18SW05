7ae7e7932947813d5f1ff0aed4791cb065821eaf
Aliza05 <49370345+Aliza05@users.noreply.github.com>
2019-04-07 08:56:10 -0700

    Add files via upload

---
+++ OOP Lab 1/SecondProgram.java
@@ -0,0 +1,12 @@
+class SecondProgram
+{
+public static void main(String args[])
+{
+  double a=50.25;
+  System.out.println("Double="+a);
+  int b=(int)a;
+  System.out.println("Integer="+b);
+  float c=(float)(a-b);
+  System.out.println("Float="+c);
+}
+}
