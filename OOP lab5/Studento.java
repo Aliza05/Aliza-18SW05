ba4375ade1a81e8ecf798e0b9840b2dc24ccf8b6
Aliza05 <49370345+Aliza05@users.noreply.github.com>
2019-05-05 08:27:59 -0700

    Add files via upload

---
+++ OOP lab5/Studento.java
@@ -0,0 +1,10 @@
+public class Studento
+{
+ public static void main(String args[])
+ {
+  Studenti S= new Studenti();
+  S.Student("Aliza" , 18);
+  S.setData(05, "Software", 18, "MUET");
+  S.showData();
+ }
+}
